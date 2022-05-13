431ab433230594f79c237c4ec8d14fc0831dbecf
Andrey Smirnov <spiehdid@mail.ru>
2022-05-13 21:22:26 +0300

    lesson 1 update

--- lesson-1.js
+++ lesson-1.js
@@ -11,14 +11,21 @@ const isPrime = (number) => {
 };
 
 let count = 1;
+let primeCount = 0;
 
-const from = process.argv[2];
-const to = process.argv[3];
+const from = Number(process.argv[2]);
+const to = +process.argv[3];
+
+if (!(isFinite(from) && isFinite(to))) {
+  console.error(colors.red("Входные параметры должны быть числами!!!"));
+  process.exit(1);
+}
 
 for (let number = from; number <= to; number++) {
   let colorer = colors.green;
 
   if (isPrime(number)) {
+    primeCount++;
     if (count % 2 === 0) {
       colorer = colors.yellow;
       count += 1;
@@ -31,4 +38,8 @@ for (let number = from; number <= to; number++) {
 
     console.log(colorer(number));
   }
+}
+
+if (primeCount === 0) {
+  console.error(colors.red("В последовательности нет простых чисел!!!"));
 }
